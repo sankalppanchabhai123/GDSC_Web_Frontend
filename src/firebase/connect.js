66a2ce39001eabc685ea3ffe6359bca015803dee
Sankalp Panchabhai <147154199+sankalppanchabhai123@users.noreply.github.com>
2025-10-14 19:23:29 +0530

    Update connect.js

--- src/firebase/connect.js
+++ src/firebase/connect.js
@@ -1,4 +1,5 @@
 // Import the functions you need from the SDKs you need
+import "dotenv/config.js"
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
